210da77c138d7fa6ba5d51efc25a54974a585216
NIMA <linux.vps.vpn@gmail.com>
2024-10-04 19:22:25 -0700

    add service

--- src/app/service/prediction/prediction.service.ts
+++ src/app/service/prediction/prediction.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../../environments/environment';
+import {Constants} from '../../constants/constants';
+
+
+
+export interface CashPrediction {
+  actualCash: number;
+  predictedCash: number;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class PredictionService {
 
-  constructor() { }
+  constructor(private http:  HttpClient) {}
+
+  getPredictions(id: number)
+  {
+    const url = `${environment.API_BASE_URL}${Constants.PREDICTIONS_API_URL}/${id}`;
+    console.log(url);
+    return this.http.get(url, {observe: 'response'});
+  }
 }
