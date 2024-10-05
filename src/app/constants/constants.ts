38f199894889dc77b5054da26c978a3bc8da2138
NIMA <linux.vps.vpn@gmail.com>
2024-10-04 18:32:02 -0700

    add component prediction-view

---
+++ src/app/constants/constants.ts
@@ -0,0 +1,8 @@
+export const Constants = {
+
+  GET_ALL_CARDS_API_URL: "/getAllCards",
+  GET_CONTACT_US_API_URL: "/contactus",
+  SIGN_UP_USER_API_URL: "/login/register",
+  PREDICTIONS_API_URL: "api/predictions"
+
+}
