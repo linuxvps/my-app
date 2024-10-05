9a78fe3a41aab0958395ddb13d95fc8380e4accf
NIMA <linux.vps.vpn@gmail.com>
2024-10-04 20:06:39 -0700

    all change done

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
 	production:true,
-	// API_BASE_URL: 'http://77.221.158.94:8080/'
-	API_BASE_URL: 'http://localhost:8089/'
+	API_BASE_URL: 'http://77.221.158.94:8080/'
 }
