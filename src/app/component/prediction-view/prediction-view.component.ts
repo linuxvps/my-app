d6a11f6961cfd78186832f56ccd4e65f9bafa139
NIMA <linux.vps.vpn@gmail.com>
2024-10-04 19:34:45 -0700

    add

--- src/app/component/prediction-view/prediction-view.component.ts
+++ src/app/component/prediction-view/prediction-view.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // Add this import
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-prediction-view',
-  standalone: true,
-  imports: [],
   templateUrl: './prediction-view.component.html',
+  standalone: true,
+  imports: [CommonModule, FormsModule], // Add FormsModule to imports
   styleUrl: './prediction-view.component.scss'
 })
-export class PredictionViewComponent {
+export class PredictionViewComponent implements OnInit, AfterViewInit {
 
+  ngOnInit(): void {
+
+  }
+
+  ngAfterViewInit(): void {
+
+  }
 }
+
+
