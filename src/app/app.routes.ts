2da4dd29cca676d4c61afde106e9b48bd860bf08
NIMA <linux.vps.vpn@gmail.com>
2024-10-05 23:28:23 -0700

    change nginx polyfills

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,5 @@ import { Routes } from '@angular/router';
 import {PredictionViewComponent} from './component/prediction-view/prediction-view.component';
 
 export const routes: Routes = [
-  {path: 'pre', component: PredictionViewComponent}
+  {path: '', component: PredictionViewComponent}
 ];
